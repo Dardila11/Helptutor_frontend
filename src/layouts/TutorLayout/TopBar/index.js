b210dbbf479efac11a856410dbe9a8e41f102723
Dardila11 <danieleardila@hotmail.com>
2021-04-08 00:03:03 -0400

    fixed exit icon color

--- src/layouts/TutorLayout/TopBar/index.js
+++ src/layouts/TutorLayout/TopBar/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Link as RouterLink, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 import {
   Toolbar,
   Typography,
@@ -8,7 +8,7 @@ import {
   IconButton
 } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import TutorNavBar from '../NavBar'
 import logo from '../logo.svg'
 
@@ -43,73 +43,84 @@ const useStyles = makeStyles((theme) => ({
     border: '0px',
     color: 'white'
   },
-  userSection:{
+  userSection: {
     color: 'white',
-    margin : theme.spacing(0)
+    margin: theme.spacing(0)
   },
   userSpace: {
-    color : theme.palette.common.white,
-    marginRight : theme.spacing(1)
+    color: theme.palette.common.white,
+    marginRight: theme.spacing(1)
   },
   toolbar: {
     backgroundColor: theme.palette.primary.main
   },
   img: {
     color: theme.palette.common.white
+  },
+  logout: {
+    marginRight: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+    color: theme.palette.common.white
+  },
+  exitIcon: {
+    marginLeft: theme.spacing(1)
   }
 }))
 
 const TutorTopBar = (props) => {
   const classes = useStyles()
-  
+
   const handleLogOut = () => {
     props.logout()
   }
 
   return (
     <div className={classes.grow}>
-        <Toolbar className={classes.toolbar}color="primary" >
-          <Button className={classes.button} variant="outlined" href="/">
-            <img className={classes.img}
-              src={logo}
-              width="50"
-              alt="LogoImage"></img>
-            <Typography
-              className={classes.title}
-              variant="h2"
-              color="initial"
-              noWrap>
-              HELPTUTOR
-            </Typography>
-          </Button>
-          <TutorNavBar/>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <RouterLink to="/tutor/cuenta">
-              <div className={classes.userSection}>
-                <IconButton
-                  className={classes.userSpace}
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  color="inherit">
-                  <Typography className={classes.userSpace} variant="h4">
-                    {props.user!=null? (
-                        <>{props.user.first_name} {props.user.last_name}</>
-                    ):(
-                      <></>
-                    )}
-                    
-                  </Typography>
-                  <AccountCircle />
-                </IconButton>
-              </div>
-            </RouterLink>
-            <Button endIcon={<ExitToAppIcon/>} onClick={handleLogOut}>
-                  Salir
-            </Button>
-          </div>
-        </Toolbar>
+      <Toolbar className={classes.toolbar} color="primary">
+        <Button className={classes.button} variant="outlined" href="/">
+          <img
+            className={classes.img}
+            src={logo}
+            width="50"
+            alt="LogoImage"></img>
+          <Typography
+            className={classes.title}
+            variant="h2"
+            color="initial"
+            noWrap>
+            HELPTUTOR
+          </Typography>
+        </Button>
+        <TutorNavBar />
+        <div className={classes.grow} />
+        <div className={classes.sectionDesktop}>
+          <RouterLink to="/tutor/cuenta">
+            <div className={classes.userSection}>
+              <IconButton
+                className={classes.userSpace}
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit">
+                <Typography className={classes.userSpace} variant="h4">
+                  {props.user != null ? (
+                    <>
+                      {props.user.first_name} {props.user.last_name}
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </Typography>
+                <AccountCircle />
+              </IconButton>
+            </div>
+          </RouterLink>
+          <IconButton onClick={handleLogOut} className={classes.logout}>
+            <Typography variant="h4">Salir</Typography>
+            <ExitToAppIcon className={classes.exitIcon} />
+          </IconButton>
+        </div>
+      </Toolbar>
     </div>
   )
 }
@@ -120,6 +131,5 @@ const mapStateToProps = (state) => ({
 })
 
 export default connect(mapStateToProps, {
-logout
+  logout
 })(TutorTopBar)
-
