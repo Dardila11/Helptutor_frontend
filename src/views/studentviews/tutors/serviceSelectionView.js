eea05b908e6ecf12b38f9955b98848868a7fbae4
Dardila11 <danieleardila@hotmail.com>
2021-09-06 09:00:49 -0400

    improve dialog service selection view design

--- src/views/studentviews/tutors/serviceSelectionView.js
+++ src/views/studentviews/tutors/serviceSelectionView.js
@@ -10,10 +10,14 @@ import {
   Box,
   Card,
   DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
   Grid,
   IconButton
 } from '@material-ui/core'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import CloseIcon from '@material-ui/icons/Close'
 import ProfileView from 'src/components/cards/tutorProfileCard'
 import Schedule from 'src/components/Schedule/Schedule'
 
@@ -49,6 +53,10 @@ const useStyles = makeStyles((theme) => ({
   card: {
     margin: theme.spacing(1),
     borderRadius: '20px'
+  },
+  centerStepper: {
+    display: 'flex',
+    justifyContent: 'center'
   }
 }))
 
@@ -95,142 +103,165 @@ const ServiceSelectionView = (props) => {
   }
 
   return (
-    <div className={classes.root}>
-      <DialogTitle id="publications-dialog-title" align="center">
-        <Typography className={classes.instructions}>
-          {getStepContent(activeStep)}
-        </Typography>
+    <>
+      <DialogTitle
+        id="publications-dialog-title"
+        align="center"
+        onClose={props.onClose}>
+        <Box display="flex" alignItems="center">
+          <Box flexGrow={1}>
+            <Typography variant="h3" className={classes.instructions}>
+              {getStepContent(activeStep)}
+            </Typography>
+          </Box>
+          <IconButton onClick={props.onClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
       </DialogTitle>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center">
-        <Grid container>
-          <Grid item xs={1}>
-            {activeStep > 0 ? (
-              <IconButton onClick={handleBack}>
-                <ArrowBackIosIcon />
-              </IconButton>
-            ) : (
-              <></>
-            )}
-          </Grid>
-          <Grid item xs={10}>
-            {activeStep === 0 ? (
-              <>
-                <ProfileView idTutor={idTutor} next={handleTutor} />
-              </>
-            ) : (
-              <></>
-            )}
-            {activeStep === 1 ? (
-              <>
-                <Schedule next={handleSchedule} />
-              </>
-            ) : (
-              <></>
-            )}
-            {activeStep === 2 ? (
-              <>
-                <Box
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="center"
-                  alignItems="center">
-                  <Box>
-                    <Typography variant="h4" textAlign="center">
-                      Información del servicio a contratar
-                    </Typography>
-                  </Box>
-                  <Card className={classes.card} elevation={3}>
-                    <Box className={classes.contractInformation}>
-                      <Box display="flex" justifyContent="center">
-                        <Avatar
-                          className={classes.cover}
-                          alt="user photo"
-                          src="/static/images/avatars/avatar_6.png"
-                        />
-                      </Box>
+      <DialogContent dividers>
+        <DialogContentText id="scroll-dialog-description" tabIndex={-1}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center">
+            <Grid container>
+              <Grid item xs={1}>
+                {activeStep > 0 ? (
+                  <IconButton onClick={handleBack}>
+                    <ArrowBackIosIcon />
+                  </IconButton>
+                ) : (
+                  <></>
+                )}
+              </Grid>
+              <Grid item xs={10}>
+                {activeStep === 0 ? (
+                  <>
+                    <ProfileView idTutor={idTutor} next={handleTutor} />
+                  </>
+                ) : (
+                  <></>
+                )}
+                {activeStep === 1 ? (
+                  <>
+                    <Schedule next={handleSchedule} />
+                  </>
+                ) : (
+                  <></>
+                )}
+                {activeStep === 2 ? (
+                  <>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      justifyContent="center"
+                      alignItems="center">
                       <Box>
-                        <Typography>
-                          <b>Tutor:</b> {contract.tutor.user.first_name}{' '}
-                          {contract.tutor.user.last_name}
-                        </Typography>
-                        <Typography>
-                          <b>Servicio:</b> {service.title}
-                        </Typography>
-                        <Typography>
-                          <b>Precio:</b> {service.price}$
-                        </Typography>
-                        <Typography>
-                          <b>Franja:</b> {contract.slot.day} de{' '}
-                          {contract.slot.start <= 12
-                            ? contract.slot.start
-                            : contract.slot.start - 12}{' '}
-                          {contract.slot.start < 12 ? 'am' : 'pm'} a{' '}
-                          {contract.slot.end <= 12
-                            ? contract.slot.end
-                            : contract.slot.end - 12}{' '}
-                          {contract.slot.end < 12 ? 'am' : 'pm'}
+                        <Typography variant="h4" textAlign="center">
+                          Información del servicio a contratar
                         </Typography>
                       </Box>
+                      <Card className={classes.card} elevation={3}>
+                        <Box className={classes.contractInformation}>
+                          <Box display="flex" justifyContent="center">
+                            <Avatar
+                              className={classes.cover}
+                              alt="user photo"
+                              src="/static/images/avatars/avatar_6.png"
+                            />
+                          </Box>
+                          <Box>
+                            <Typography>
+                              <b>Tutor:</b> {contract.tutor.user.first_name}{' '}
+                              {contract.tutor.user.last_name}
+                            </Typography>
+                            <Typography>
+                              <b>Servicio:</b> {service.title}
+                            </Typography>
+                            <Typography>
+                              <b>Precio:</b> {service.price}$
+                            </Typography>
+                            <Typography>
+                              <b>Franja:</b> {contract.slot.day} de{' '}
+                              {contract.slot.start <= 12
+                                ? contract.slot.start
+                                : contract.slot.start - 12}{' '}
+                              {contract.slot.start < 12 ? 'am' : 'pm'} a{' '}
+                              {contract.slot.end <= 12
+                                ? contract.slot.end
+                                : contract.slot.end - 12}{' '}
+                              {contract.slot.end < 12 ? 'am' : 'pm'}
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Card>
                     </Box>
-                  </Card>
-                </Box>
-                <Box className={classes.nextButton}>
-                  <form
-                    method="post"
-                    action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
-                    <input name="merchantId" type="hidden" value={508029} />
-                    <input name="accountId" type="hidden" value={512321} />
-                    <input
-                      name="ApiLogin"
-                      type="hidden"
-                      value="pRRXKOl8ikMmt9u"
-                    />
-                    <input name="description" type="hidden" value="Test PAYU" />
-                    <input
-                      name="referenceCode"
-                      type="hidden"
-                      value="TestPayU"
-                    />
-                    <input name="amount" type="hidden" value={3} />
-                    <input name="tax" type="hidden" value={0} />
-                    <input name="taxReturnBase" type="hidden" value={0} />
-                    <input name="currency" type="hidden" value="COP" />
-                    <input
-                      name="signature"
-                      type="hidden"
-                      value="ba9ffa71559580175585e45ce70b6c37"
-                    />
-                    <input name="test" type="hidden" value="1" />
-                    <input
-                      name="buyerEmail"
-                      type="hidden"
-                      value="test@test.com"
-                    />
-                    <input
-                      name="responseUrl"
-                      type="hidden"
-                      value="http://www.test.com/response"
-                    />
-                    <input
-                      name="confirmationUrl"
-                      type="hidden"
-                      value="http://www.test.com/confirmation"
-                    />
-                    <Button variant="contained" color="primary" type="submit">
-                      Ir al pago
-                    </Button>
-                  </form>
-                </Box>
-              </>
-            ) : (
-              <></>
-            )}
-          </Grid>
-        </Grid>
+                    <Box className={classes.nextButton}>
+                      <form
+                        method="post"
+                        action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
+                        <input name="merchantId" type="hidden" value={508029} />
+                        <input name="accountId" type="hidden" value={512321} />
+                        <input
+                          name="ApiLogin"
+                          type="hidden"
+                          value="pRRXKOl8ikMmt9u"
+                        />
+                        <input
+                          name="description"
+                          type="hidden"
+                          value="Test PAYU"
+                        />
+                        <input
+                          name="referenceCode"
+                          type="hidden"
+                          value="TestPayU"
+                        />
+                        <input name="amount" type="hidden" value={3} />
+                        <input name="tax" type="hidden" value={0} />
+                        <input name="taxReturnBase" type="hidden" value={0} />
+                        <input name="currency" type="hidden" value="COP" />
+                        <input
+                          name="signature"
+                          type="hidden"
+                          value="ba9ffa71559580175585e45ce70b6c37"
+                        />
+                        <input name="test" type="hidden" value="1" />
+                        <input
+                          name="buyerEmail"
+                          type="hidden"
+                          value="test@test.com"
+                        />
+                        <input
+                          name="responseUrl"
+                          type="hidden"
+                          value="http://www.test.com/response"
+                        />
+                        <input
+                          name="confirmationUrl"
+                          type="hidden"
+                          value="http://www.test.com/confirmation"
+                        />
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit">
+                          Ir al pago
+                        </Button>
+                      </form>
+                    </Box>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </Grid>
+            </Grid>
+          </Box>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions classes={{ root: classes.centerStepper }}>
         <Stepper activeStep={activeStep}>
           {steps.map((label, index) => {
             const stepProps = {}
@@ -242,8 +273,8 @@ const ServiceSelectionView = (props) => {
             )
           })}
         </Stepper>
-      </Box>
-    </div>
+      </DialogActions>
+    </>
   )
 }
 
