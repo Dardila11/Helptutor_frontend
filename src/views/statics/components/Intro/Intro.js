13a1431111f166100ffcadc6c52d45cf33379cb3
Dardila11 <danieleardila@hotmail.com>
2021-10-02 11:36:28 -0400

    fixed ui issues

--- src/views/statics/components/Intro/Intro.js
+++ src/views/statics/components/Intro/Intro.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-//import Student from 'src/views/statics/components/Intro/student.jpeg'
 import './style.css'
 
 const Intro = () => {
   return (
     <div className="sec1--container">
-      <h2 className="sec1--h2">Tutorias especializadas en linea</h2>
+      <h2 className="sec1--h2">Tutorias especializadas en línea</h2>
       <p className="sec1--p">
-        Si necesitas conocimiento en algún tema en especifico, nuestra
+        Si necesitas conocimiento en algún tema en específico, nuestra
         plataforma HelpTutor es nuestra solución a tus problemas.
       </p>
     </div>
