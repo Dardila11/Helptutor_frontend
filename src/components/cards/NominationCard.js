9efd86cae4eb1aa626caaebd2ddddd91f8d756a8
alexvi <andyvilla@unicauca.edu.co>
2021-10-03 23:05:11 -0500

    selection view update

--- src/components/cards/NominationCard.js
+++ src/components/cards/NominationCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import {
@@ -18,8 +17,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     borderRadius: '20px',
-    height: 200,
-    width: 250
+  },
+  cardAction: {
+    borderRadius: '20px',
+    minHeight: 200,
+    minWidth: 288,
   },
   userSpace: {
     display: 'flex',
@@ -28,23 +30,16 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     marginTop: theme.spacing(1)
   },
-  details: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
   content: {
-    flex: '1 0 auto'
+    flex: '1 0 auto',
+    minHeight: 100
   },
   cover: {
     width: 40,
     height: 40
   },
   paper: {
-    borderRadius: '20px',
-    margin: theme.spacing(1)
-  },
-  methodology: {
-    marginTop: theme.spacing(1)
+    borderRadius: '20px'
   },
   price: {
     color: '#1ad41a',
@@ -62,7 +57,6 @@ const NominationCard = (props) => {
   }
   return (
     <Paper className={classes.paper} elevation={3}>
-      <Card className={classes.root}>
         <CardActionArea className={classes.cardAction} onClick={handleSelect}>
           <Grid container>
             <Grid item xs={6}>
@@ -88,14 +82,13 @@ const NominationCard = (props) => {
             </Grid>
             <Grid item xs={12}>
               <CardContent className={classes.content}>
-                <Typography component={'span'} color="textSecondary" variant="h5">
+                <Typography component={'span'} color="textSecondary" variant="h6">
                   {nomination.description}
                 </Typography>
               </CardContent>
             </Grid>
           </Grid>
         </CardActionArea>
-      </Card>
     </Paper>
   )
 }
